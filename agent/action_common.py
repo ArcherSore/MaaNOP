d8ac85a0e243f9a9e6f11a72a5ad1405e435a4d4
ArcherSore <17321322251@163.com>
2026-09-06 16:33:22 +0800

    revert: 移除 ResetServerRuntime 避免 clear_cache 清除 task_id 映射

--- agent/action_common.py
+++ agent/action_common.py
@@ -2,7 +2,7 @@ from maa.agent.agent_server import AgentServer
 from maa.custom_action import CustomAction
 from maa.context import Context
 
-from common import click_box_center, send_focus_message
+from common import click_box_center
 
 
 @AgentServer.custom_action("PreciseClick")
@@ -14,13 +14,3 @@ class PreciseClick(CustomAction):
     ) -> bool:
         return click_box_center(context, argv.box)
 
-
-@AgentServer.custom_action("ResetServerRuntime")
-class ResetServerRuntime(CustomAction):
-    def run(
-        self,
-        context: Context,
-        argv: CustomAction.RunArg,
-    ) -> bool:
-        context.tasker.clear_cache()
-        return True
